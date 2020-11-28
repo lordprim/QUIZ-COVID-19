4a3e708b29d947411488e1f91a25b4a3700f1194
Vitor Prim <59850339+lordprim@users.noreply.github.com>
2020-11-27 21:59:26 -0300

    Add files via upload

---
+++ question.js
@@ -0,0 +1,55 @@
+
+// array of objects
+const quiz = [
+    {
+        q:'Onde foi identificado o primeiro caso de COVID ?',
+        options: ['França', 'Inglaterra', 'China', 'Brasil'],
+        answer:2
+    },
+    {
+        q:'Qual foi a primeira cidade a ter caso de Corona Vírus ?',
+        options: ['Wuhan', 'Roma', 'Florianópolis', 'Rio de Janeiro'],
+        answer:0
+    },
+    {
+        q:'Em qual ano foi confirmado o primeiro caso da doença ?',
+        options: ['2017', '2018', '2020', '2019'],
+        answer:3
+    },
+    {
+        q:'Qual desses sintomas o paciente do COVID-19 sente?',
+        options: ['Dor no polegar', 'Dor nas costas', 'Febre', 'Formigação na mão'],
+        answer:2
+    },
+    {
+        q:'Quem é mais propissio a pegar COVID ?',
+        options: ['Recém-nascidos', 'Adolescentes', 'Crianças', 'Idosos'],
+        answer:3
+    },
+    {
+        q:'A transmissão do Corona Vírus acontece de qual forma ?',
+        options: ['Distanciamento', 'Home Office', 'Contato próximo', 'Higienização'],
+        answer:2
+    },
+    {
+        q:'Mascara é importante ?',
+        options: ['Não', 'Sim'],
+        answer:1
+    },
+    {
+        q:'País com maior número de mortes por COVID-19 ?',
+        options: ['Peru', 'Brasil', 'Bélgica', 'Bolivia'],
+        answer:0
+    },
+    {
+        q:'Porque "Corona" vírus ?',
+        options: ['Pelo formato', 'Pelo nome de quem o descobriu', 'Intuição', 'Por decisão unânime'],
+        answer:0
+    },
+    {
+        q:'A quarentena são quantos dias ?',
+        options: ['23', '13', '17', '14'],
+        answer:3
+    }
+
+]
